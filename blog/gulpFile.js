7e5a0c5dc6681f7796c7295a5edef2146d790b61
Agostinho Ramos <agostinhopina095@gmail.com>
2020-09-16 16:21:58 +0100

    ui dependencies
    npm rebuild --force
    npm i bootstrap font-awesome --save
    touch gulpFile.js
    npm install --save-dev gulp-clean-css gulp-cli gulp gulp-sass
    npm i react-redux redux redux-thunk react-router-dom axios

---
+++ blog/gulpFile.js
@@ -0,0 +1,49 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cleanCSS = require("gulp-clean-css");
+const { dest } = require("gulp");
+
+var paths = {
+  styles: {
+    src: "node_modules/bootstrap/dist/css/*.css",
+    dest: "public",
+  },
+  script: {
+    src: ["node_modules/bootstrap/dist/js/bootstrap.js"],
+    dest: "public",
+  },
+  scss: {
+    src: [
+      "node_modules/bootstrap/scss/bootstrap.scss",
+      "node_modules/font-awesome/scss/font-awesome.scss",
+    ],
+    dest: "public",
+  },
+  folder: {
+    font_awesome: {
+      src: "node_modules/font-awesome/fonts/*",
+      dest: "public",
+    },
+  },
+  image: [],
+};
+
+gulp.task("buildCSS", function () {
+  return gulp
+    .src(paths.scss.src)
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(cleanCSS()) // Minify CSS files..
+    .pipe(gulp.dest(paths.scss.dest));
+});
+
+gulp.task("buildJS", function () {
+  return gulp.src(paths.script.src).pipe(gulp.dest(paths.script.dest));
+});
+
+gulp.task("copyFolder", function () {
+  return gulp
+    .src(paths.folder.font_awesome.src)
+    .pipe(gulp.dest(paths.folder.font_awesome.dest));
+});
+
+gulp.task("main", gulp.series("buildCSS", "buildJS", "copyFolder"));
